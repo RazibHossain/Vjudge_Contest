8283f25b17302c492f4974653e43413ac8475cdb
RazibHossain <asdf130126@gmail.com>
2020-07-14 07:56:28 +0600

    uva problem

---
+++ 11044 Searching for Nessy.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int t;cin>>t;
+    for(int i=1;i<=t;i++){
+
+            int n,m;cin>>n>>m;
+            int nn=n/3;
+            int mm=m/3;
+            cout<<nn*mm<<endl;
+
+    }
+
+
+}
+
---
+++ Celebrity jeopardy.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    char x[10];
+        while(gets(x))
+    puts(x);
+
+}
